130e6739ad3ed83487c25e9f4fcb6056b95ac9aa
nicolas <nicolas@kali>
2024-08-12 23:34:46 +0200

    i  use $1 & $2

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 ##!/bin/bash
-sudo whois $1 | awk -F ',' '/^Registrant|^Admin|^Tech/{gsub(":", ",");print $0}' > $1.csv !/bin/bash
+sudo whois $1 | awk -F":"' /^(Registrant|Admin|Tech)/ print $1 "," $2}' > $1.csv
