314d01711698e03ee5fdaa0fb18c8f6549ee202a
nicolas <nicolas@kali>
2024-08-12 23:30:10 +0200

    update whois command

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-whois $1 | awk -F ' /Registrant|Admin|Tech|registrant|admin|tech/' > $1.csv
+##!/bin/bash
+sudo whois $1 | awk -F ',' '/^Registrant|^Admin|^Tech/{gsub(":", ",");print $0}' > $1.csv !/bin/bash
