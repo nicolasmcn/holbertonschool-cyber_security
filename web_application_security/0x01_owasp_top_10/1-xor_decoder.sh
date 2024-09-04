e1c82fb129a926af815311806aa867b2632d858c
nicolas <nicolas@kali>
2024-09-05 01:20:57 +0200

    new script with loop

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "$1" | sed 's/{xor}//' | base64 --decode | tr '\0-\177' '\060-\137'
+
+encoded_hash=$(echo "$1" | sed 's/{xor}//')
+
+decoded_base64=$(echo "$encoded_hash" | base64 --decode)
+
+XOR_KEY=0x5A
+
+decoded=""
+
+for (( i=0; i<${#decoded_base64}; i++ )); do
+    char="${decoded_base64:$i:1}"
+
+    dec=$(printf '%d' "'$char")
+
+    decoded_char=$(printf \\x$(printf %x $((dec ^ XOR_KEY))))
+
+    decoded+="$decoded_char"
+done
+
+echo "$decoded"
+
