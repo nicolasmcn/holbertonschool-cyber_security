12c23856e53425fd7a7996a5fc565de101afc075
nicolas <nicolas@kali>
2024-10-09 16:21:03 +0200

    add a awk to supp ?:

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=rockyou.txt --format=Raw-MD5 "$1" && john --show --format=Raw-MD5 "$1" > 4-password.txt
+john --wordlist=rockyou.txt --format=Raw-MD5 "$1" && john --show --format=Raw-MD5 "$1" | awk -F':' '{print $2}' > 4-password.txt
