9692a5a01bb9ca97b9083290fb2a4a9e4bc749a6
nicolasmcn <n.mancino03@gmail.com>
2024-10-16 18:09:49 +0200

    all in 1 line

--- linux_security/0x02_mandatory_access_control/3-add_port.sh
+++ linux_security/0x02_mandatory_access_control/3-add_port.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo semanage port -a -t http_port_t -p tcp 81
-sudo semanage port -l | grep "http_port_t"
+sudo semanage port -a -t http_port_t -p tcp 81 |sudo semanage port -l | grep "http_port_t"
