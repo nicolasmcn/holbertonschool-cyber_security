97fe5dacf965db56cde186c5548a48b2fc387159
nicolasmcn <n.mancino03@gmail.com>
2024-10-17 13:51:52 +0200

    that activate alone

--- linux_security/0x02_mandatory_access_control/7-set_sendmail.sh
+++ linux_security/0x02_mandatory_access_control/7-set_sendmail.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo setsebool -P httpd_can_sendmail "$1" | sudo semanage boolean -l | grep "httpd_can_sendmail"
+sudo setsebool -P httpd_can_sendmail on | sudo semanage boolean -l | grep "httpd_can_sendmail"
