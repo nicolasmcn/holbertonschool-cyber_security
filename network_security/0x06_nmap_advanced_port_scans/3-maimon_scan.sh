8cc798b178ed8a63e928c42eaebf2c22c005b0ce
nicolasmcn <n.mancino03@gmail.com>
2024-11-06 20:22:10 +0100

    pattern vv

--- network_security/0x06_nmap_advanced_port_scans/3-maimon_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/3-maimon_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -sM -v -p 21,22,23,80,443 -http -https -ssh -ftp -telnet --open --reason --packet-trace "$1"
+sudo nmap -sM -v -p 21,22,23,80,443 -http -https -ssh -ftp -telnet -vv --open --reason --packet-trace "$1"
