6b38cf2b0be9b698b40b1c8bb533fa94909495ac
nicolasmcn <n.mancino03@gmail.com>
2024-11-20 01:52:46 +0100

    without

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $12}' "logs.txt" | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}'
+awk -F\" '{print $6}' "logs.txt" | sort | uniq -c | sort -rn | head -n 1 | awk '{$1=""; print $0}' | sed 's/^ //'
