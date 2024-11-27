622037a3c5664ac65a858ff141a267cc71b29b8a
nicolasmcn <n.mancino03@gmail.com>
2024-11-27 02:20:06 +0100

    add new options

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sV -A --script "banner*,ssl-enum-ciphers*,default*,smb-enum-domains*" $1 -oN service_enumeration_results.tx
+nmap -sV -sC -A -O --script "banner*,ssl-enum-ciphers*,default*,smb-enum-domains*" $1 -oN service_enumeration_results.tx
