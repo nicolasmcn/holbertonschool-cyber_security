b0995e580466ee6d454ea6e9daba9347887f1e26
Mancino Nicolas <130404454+nicolasmcn@users.noreply.github.com>
2025-02-06 18:26:26 +0100

    try this method

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -sC -sV -A -O --traceroute --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.txt
+nmap -sV -A --script default ssl-enum-ciphers smb-enum-domains banner $1 -oN service_enumeration_results.txt
