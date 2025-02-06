8054ffb4f9ed65556dfdb4d659961b037200b1de
Mancino Nicolas <130404454+nicolasmcn@users.noreply.github.com>
2025-02-06 18:18:41 +0100

    Update file

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sV -sC -A -O -traceroute --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.tx
+sudo nmap -sC -sV -A -O --traceroute --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.txt
